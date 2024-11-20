43a7e1c41e64152a7500f55d573b8131105c67fd
javier <chico_jrf@hotmail.com>
2024-11-20 11:20:21 +0100

    feat: creo otra funcion mas concreta

--- menu_para_invitados.py
+++ menu_para_invitados.py
@@ -10,11 +10,11 @@
 Los elementos de un set son único, lo que significa que no puede haber elementos duplicados.
 """
 """ La función update() del conjunto añade elementos sin duplicados. Si algún ingrediente ya está en lista_de_la_compra, no se añadirá de nuevo."""
-
+""" El lenguaje de programación C permite usar las secuencias de escape \n (nueva línea) y \r (retorno de carro)."""
 # Diccionario
 
 menu = {
-    "Aperitivo: Guacamle": {
+    "Aperitivo: Guacamole": {
         "cilantro fresco", "cebolla", "chile fresco", "ajo", "tomate",
         "aguacates", "zumo de limon", "sal"
     },
@@ -28,7 +28,7 @@ menu = {
 
 # Funcion
 
-def lista_de_la_compra(menu):
+"""def lista_de_la_compra(menu):
     lista_de_la_compra = set() # Lo pongo para que no se usen duplicados arriba explico 
 
     for ingredientes in menu.values():
@@ -41,4 +41,25 @@ lista_compra = lista_de_la_compra(menu)
 print("Lista de la compra:")
 for ingrediente in lista_compra:
     print("-", ingrediente)
+"""
 
+def ingredientes_por_plato(menu, plato):
+    ingredientes = menu.get(plato)
+    if ingredientes:
+        print(f"\nIngredientes necesarios para {plato}:")
+        for ingrediente in ingredientes:
+            print("-", ingrediente)
+    else :
+        print(f"\nNo se encontró el plato '{plato}' en el menú.")
+
+def interactuar_con_usuario(menu):
+    # Imprimo mensaje de introduccion
+    print ("""\nEstás preparando un gran banquete para tu gente en casa
+            y quieres  preparar el menú y hacer la lista de la compra. El menú está formado por:\n""")
+    
+    print("Platos disponibles:")
+    for plato in menu.keys():
+        print("-", plato)
+
+    plato_seleccionado = input("¿Qué plato quieres cocinar? ")
+    ingredientes_por_plato(menu, plato_seleccionado)
