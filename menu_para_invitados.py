d7986d08b3abfa5fedb9cc0b8a0e5a9591e97bab
javier <chico_jrf@hotmail.com>
2024-11-20 10:40:13 +0100

    feat: diccionario creado

---
+++ menu_para_invitados.py
@@ -0,0 +1,32 @@
+
+
+""" Estás preparando un gran banquete para tus mejores amistades en casa y quieres
+ hacer un programa para preparar el menú y hacer la lista de la compra.
+ El menú está formado por:
+ """
+
+# Diccionario
+
+menu = {
+    "Aperitivo: Guacamle": {
+        "cilantro fresco", "cebolla", "chile fresco", "ajo", "tomate",
+        "aguacates", "zumo de limon", "sal"
+    },
+    "Primer plato: Pimientos del piquillo rellenos": {
+        "huevo duro", "palitos de cangrejo", "atún en aceite de oliva", "mayonesa", "pimientos del piquillo", "pimiento verde",
+        "pimiento rojo", "cebolla", "vinagre de vino", "sal", "aceite de oliva"
+    },
+    "Postre: Brownie de chocolate con helado de turrón": { "chocolate amargo", "mantequilla sin sal", "azúcar", "huevos", "sal fina", "harina de trigo", "nueces"
+}
+}
+
+
+
+
+Con esa estructura de datos, crea una función que cree la lista de la compra.
+
+Ayuda a la implementación
+
+Puedes usar un diccionario para el menú y los platos, de forma que los ingredientes de cada plato sea un conjunto de ingredientes.
+
+Para simplificar utiliza solamente el nombre del ingrediente en singular y sin cantidades.
